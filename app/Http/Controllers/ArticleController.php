4920d64880899ebecf40e5849dfff0423cdb73ce
Carlos Souza <carlossouza@Confsistemas-MacBook-Air.local>
2023-07-17 22:06:48 -0300

    added routes for articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
         //
+        return response('Hello, World Laravel!');
     }
 
     /**
