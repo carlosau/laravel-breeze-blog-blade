430954b90e0c9b73951eb1c9a63cf36a590b800a
Carlos Souza <carlossouza@Confsistemas-MacBook-Air.local>
2023-07-18 15:27:46 -0300

    added email notification for every new article created

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ArticleCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,10 @@ class Article extends Model
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ArticleCreated::class,
     ];
 
     public function user(): BelongsTo
